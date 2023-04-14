a38d97ef90d61654fede1fa014b7f5a1157294f5
Nguyen Tran <thainguyentran@icloud.com>
2023-04-14 17:39:13 +0700

    correct code in urls

--- poll_project/poll_project/urls.py
+++ poll_project/poll_project/urls.py
@@ -17,12 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from poll import views as poll_views
+from poll_project.poll import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', poll_views.home, name='home'),
-    path('create/', poll_views.create, name='create'),
-    path('results/', poll_views.results, name='results'),
-    path('vote/', poll_views.vote, name='vote'),
+    path('', views.home, name='home'),
+    path('create/', views.create, name='create'),
+    path('results/', views.results, name='results'),
+    path('vote/', views.vote, name='vote'),
 ]
