6d061d4e713663a3ae7c55c33bf7a2865b8c6988
Nguyen Tran <thainguyentran@icloud.com>
2023-04-17 16:56:42 +0700

    update the code

--- poll_project/poll/views.py
+++ poll_project/poll/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render, redirect
-from .forms import CreatePollForm
+from django.http import HttpResponse
 
+from .forms import CreatePollForm
+from .models import Poll
 
 def home(request):
-    context = {}
+    polls = Poll.objects.all()
+    context = {
+        'polls' : polls
+    }
     return render(request, 'poll/home.html', context)
 
 def create(request):
@@ -14,14 +19,38 @@ def create(request):
             return redirect('home')
     else:
         form = CreatePollForm()
-
-    context = {'form': form}
+    context = {
+        'form' : form
+    }
     return render(request, 'poll/create.html', context)
 
-def results(request):
-    context = {}
-    return render(request, 'poll/results.html', context)
+def vote(request, poll_id):
+    poll = Poll.objects.get(pk=poll_id)
 
-def vote(request):
-    context = {}
+    if request.method == 'POST':
+
+        selected_option = request.POST['poll']
+        if selected_option == 'option1':
+            poll.option_one_count += 1
+        elif selected_option == 'option2':
+            poll.option_two_count += 1
+        elif selected_option == 'option3':
+            poll.option_three_count += 1
+        else:
+            return HttpResponse(400, 'Invalid form')
+
+        poll.save()
+
+        return redirect('results', poll.id)
+
+    context = {
+        'poll' : poll
+    }
     return render(request, 'poll/vote.html', context)
+
+def results(request, poll_id):
+    poll = Poll.objects.get(pk=poll_id)
+    context = {
+        'poll' : poll
+    }
+    return render(request, 'poll/results.html', context)
--- poll_project/poll_project/urls.py
+++ poll_project/poll_project/urls.py
@@ -18,10 +18,11 @@ from django.contrib import admin
 from django.urls import path
 
 from poll import views as poll_views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', poll_views.home, name='home'),
     path('create/', poll_views.create, name='create'),
-    path('results/', poll_views.results, name='results'),
-    path('vote/', poll_views.vote, name='vote'),
+    path('results/<poll_id>/', poll_views.results, name='results'),
+    path('vote/<poll_id>/', poll_views.vote, name='vote'),
 ]
