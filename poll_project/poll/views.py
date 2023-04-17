0d3e54fb12e9f8e3f9a6f52c48d0c92a7c32d6c2
Nguyen Tran <thainguyentran@icloud.com>
2023-04-17 11:20:59 +0700

    add forms.py to the poll

--- poll_project/poll/views.py
+++ poll_project/poll/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render
+from .forms import CreatePollForm
 
-# Create your views here.
-from django.shortcuts import render
 
 def home(request):
     context = {}
     return render(request, 'poll/home.html', context)
 
 def create(request):
-    context = {}
+    if request.method == 'POST':
+        form = CreatePollForm(request.POST)
+    else:
+        form = CreatePollForm()
+
+    context = {'form': form}
     return render(request, 'poll/create.html', context)
 
 def results(request):
